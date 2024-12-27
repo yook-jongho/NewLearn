0f10e9bddc77505cb2dde7f9788f2eaa8152b83b
yook-jongho <dbrwhdgh1203@naver.com>
2024-12-28 00:54:24 +0900

    setting: prettier, eslint 설정

--- newlearn/src/App.tsx
+++ newlearn/src/App.tsx
@@ -1,16 +1,17 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <div className="h-screen flex items-center justify-center bg-gray-100">
-                <button className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600">
-                    Hello, Tailwind!
-                </button>
-                <div className=" w-100px h-100px bg-black"></div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+        <button className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600">
+          Hello, Tailwind!
+        </button>
+        <div className=" w-100px h-100px bg-black"></div>
+        <div> takdjfkjdk </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
