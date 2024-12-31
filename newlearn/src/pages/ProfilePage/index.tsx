d9494ebd5e7fbf303666bcc97f3bca8e6962c7da
yook-jongho <dbrwhdgh1203@naver.com>
2024-12-31 23:42:16 +0900

    publish: 프로필 페이지 퍼블리싱

--- newlearn/src/pages/ProfilePage/index.tsx
+++ newlearn/src/pages/ProfilePage/index.tsx
@@ -1,5 +1,31 @@
+import Cardnews from '../../components/cardnews';
+import CategoryLayout from '../../components/categories/categoryLayout';
+
 const ProfilPage = () => {
-  return <div>news detail page</div>;
+  return (
+    <div>
+      <header>
+        육종호님은 지금까지 <br />
+        <b>총 10개</b>의 기사를 읽었어요!
+      </header>
+      <section>
+        <div> 그래프 </div>
+        <div> 순위 </div>
+      </section>
+      <section>
+        <CategoryLayout layout="profile" />
+      </section>
+      <section>
+        <div>
+          최근에 읽은 뉴스 <span>* 최신 20개만 저장됩니다.</span>
+        </div>
+        <Cardnews thumbnail={''} title={''} time={''} agency={''} />
+        <Cardnews thumbnail={''} title={''} time={''} agency={''} />
+        <Cardnews thumbnail={''} title={''} time={''} agency={''} />
+        <Cardnews thumbnail={''} title={''} time={''} agency={''} />
+      </section>
+    </div>
+  );
 };
 
 export default ProfilPage;
