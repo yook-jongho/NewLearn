f92eca61ae02b9220b3ac5493f837eb08ca9c666
yook-jongho <dbrwhdgh1203@naver.com>
2025-01-02 01:33:41 +0900

    "refactor: 변수명 수정 및 스타일 수정"

--- newlearn/src/components/cardnews.tsx
+++ newlearn/src/components/cardnews.tsx
@@ -1,27 +1,29 @@
 import Arrow from '../assets/arrow-right.svg?react';
 import Image from '../assets/image.svg?react';
+import { hoursSince } from '../utils/timeCalc';
 
 type CardNews = {
   thumbnail: string;
   title: string;
   time: string;
-  agency: string;
+  source: string;
 };
-const Cardnews = ({ thumbnail, title, time, agency }: CardNews) => {
-  const commonStyles = 'w-40 h-30 bg-[#EAF2FF]';
+
+const Cardnews = ({ thumbnail, title, time, source }: CardNews) => {
+  const commonStyles = 'w-32 bg-[#EAF2FF] rounded-lg';
   return (
-    <div className="flex flex-row rounded-lg border border-gray-300">
+    <div className="h-30 flex flex-row rounded-lg border border-gray-300">
       {thumbnail ? (
         <img src={thumbnail} className={commonStyles} />
       ) : (
-        <Image className="w-40 h-30 bg-[#EAF2FF] fill-[#B4DBFF]" />
+        <Image className="w-40 h-30 bg-[#EAF2FF] fill-[#B4DBFF] rounded-lg" />
       )}
-      <div className="w-full flex flex-row items-center justify-between bg-[#F8F9FE]">
-        <div className="p-2">
-          <span className="inline-block mb-2">{title}</span>
+      <div className="w-full h-32 flex flex-row items-center justify-between bg-[#F8F9FE] rounded-lg">
+        <div className="w-4/5 p-2">
+          <span className="inline-block text-sm">{title}</span>
           <div>
             <span className="text-xs text-gray-600">
-              {time}, {agency}
+              {hoursSince(time)}시간 전, {source}
             </span>
           </div>
         </div>
