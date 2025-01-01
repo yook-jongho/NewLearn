03183867a772ec40abee921d134d36cb8e5bf40c
yook-jongho <dbrwhdgh1203@naver.com>
2025-01-02 01:54:27 +0900

    feat: 스크랩페이지 퍼블리싱

---
+++ newlearn/src/pages/ScrapPage/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <span className="mb-5 text-xl font-semibold inline-block">
+        스크랩 목록
+      </span>
+    </header>
+  );
+};
+
+export default Header;
--- newlearn/src/pages/ScrapPage/index.tsx
+++ newlearn/src/pages/ScrapPage/index.tsx
@@ -1,5 +1,14 @@
+import BasicLayout from '../../layout/basicLayout';
+import Header from './Header';
+import ScrapNews from './ScrapNews';
+import data from '../../mockup/latestNewsList.json';
+
 const ScrapPage = () => {
-  return <div>news detail page</div>;
+  return (
+    <BasicLayout headerComponent={<Header />}>
+      <ScrapNews newsList={data.data.latestNews} />
+    </BasicLayout>
+  );
 };
 
 export default ScrapPage;
